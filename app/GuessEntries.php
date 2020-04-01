666b59acde48ae1ad816e6496987713316a23499
Godfrey M <godmartinz@gmail.com>
2020-03-31 17:28:25 -0700

    changed the format required in validation

--- app/GuessEntries.php
+++ app/GuessEntries.php
@@ -1,10 +1,20 @@
 <?php
 
+
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Watson\Validating\ValidatingTrait;
 
 class GuessEntries extends Model
 {
-
+    use ValidatingTrait;
+    protected $rules = [
+        'name' => 'required|unique',
+        'guess_date' => 'required|date'
+    ];
+    protected $fillable = [
+        'name',
+        'guess_date '
+    ];
 }

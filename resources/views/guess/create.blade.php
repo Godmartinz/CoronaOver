6e041408ccffeca612659f35397a9f39bda26b9d
snipe <snipe@snipe.net>
2020-04-06 16:56:40 -0700

    Added old values so that the form re-presents form data on error

--- resources/views/guess/create.blade.php
+++ resources/views/guess/create.blade.php
@@ -1,17 +1,17 @@
 @extends('layout')
-<form method="POST" action="guess/create">
+<form method="POST" action="{{ route('guess.create') }}">
     @csrf
 <div class="field">
     <label class="label">Screen Name</label>
     <div class="control">
-        <input class="input" type="name" name="name" placeholder="Enter a Screen Name">
+        <input class="input" type="name" name="name" placeholder="Enter a Screen Name" value="{{ old('name') }}">
     </div>
 </div>
 <div class="field">
-    <label class="label" for="guess date">Guess Date</label>
-        <input name=guess_date" type="date">
+    <label class="label" for="guess date">Guess Date (YYYY-MM-DD)</label>
+        <input name="guess_date" type="date" value="{{ old('guess_date') }}">
 </div>
-    =
+
         <div class="control">
             <button class="button is-link" type="submit">Submit</button>
         </div>

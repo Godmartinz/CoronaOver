7030380ec760a5ea31341136ea2f21938ed6b244
Godfrey M <godmartinz@gmail.com>
2020-04-06 14:20:44 -0700

    WIP

--- resources/views/guess/create.blade.php
+++ resources/views/guess/create.blade.php
@@ -4,12 +4,12 @@
 <div class="field">
     <label class="label">Screen Name</label>
     <div class="control">
-        <input class="input" type="name" placeholder="Enter a Screen Name">
+        <input class="input" type="name" name="name" placeholder="Enter a Screen Name">
     </div>
 </div>
 <div class="field">
-    <label class="label">Guess Date</label>
-        <input type="date">
+    <label class="label" for="guess date">Guess Date</label>
+        <input name=guess_date" type="date">
 </div>
     =
         <div class="control">

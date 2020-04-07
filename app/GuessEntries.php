1ef1cd02bb09b149bc26d27504a1b74392f6160f
snipe <snipe@snipe.net>
2020-04-06 17:17:04 -0700

    Removed created_at (since we’re using the magical ones now), added a restriction for it being after today

--- app/GuessEntries.php
+++ app/GuessEntries.php
@@ -12,16 +12,14 @@ class GuessEntries extends Model
 
     protected $rules = [
         'name' => 'required|unique',
-        'guess_date' => 'required|date',
-        'created_at' => 'date'
+        'guess_date' => 'required|date_format:Y-m-d|after:today'
     ];
+
     protected $validationMessages = [
         'name.required' => 'Dont forget your name.',
         'name.unique' => 'Someone else has your name.',
         'guess_date.required' => 'Where is your guess?',
         'guess_date.date' => 'That doesnt look like a date to me.'
-
-
     ];
 
 }

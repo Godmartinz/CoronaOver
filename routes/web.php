0c4d7e3cbf87764523c87c9692f2e794218f977f
snipe <snipe@snipe.net>
2020-04-06 17:41:17 -0700

    Removed closure for index, since we need to send it to a controller

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function(){
-    return view('index');
-})->name('home');
 
+Route::get('/',['as' => 'home','uses' => 'GuessEntriesController@index']);
 Route::post('/guess/create',['as' => 'guess.store','uses' => 'GuessEntriesController@store']);
 Route::get('/guess/{id}',['as' => 'guess.show','uses' => 'GuessEntriesController@show']);
 
